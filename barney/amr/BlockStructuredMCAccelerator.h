92f1899720c6bfd20c5fdf2a918343818b74e2a7
Ingo Wald <ingowald@gmail.com>
2024-01-14 16:21:08 -0700

    disabling deprecated code

--- barney/amr/BlockStructuredMCAccelerator.h
+++ barney/amr/BlockStructuredMCAccelerator.h
@@ -1,49 +1,49 @@
-// ======================================================================== //
-// Copyright 2023-2023 Ingo Wald                                            //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
+// // ======================================================================== //
+// // Copyright 2023-2023 Ingo Wald                                            //
+// //                                                                          //
+// // Licensed under the Apache License, Version 2.0 (the "License");          //
+// // you may not use this file except in compliance with the License.         //
+// // You may obtain a copy of the License at                                  //
+// //                                                                          //
+// //     http://www.apache.org/licenses/LICENSE-2.0                           //
+// //                                                                          //
+// // Unless required by applicable law or agreed to in writing, software      //
+// // distributed under the License is distributed on an "AS IS" BASIS,        //
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// // See the License for the specific language governing permissions and      //
+// // limitations under the License.                                           //
+// // ======================================================================== //
 
-#pragma once
+// #pragma once
 
-#include "barney/volume/MCAccelerator.h"
-#include "barney/amr/CUBQLBlockSampler.h"
+// #include "barney/volume/MCAccelerator.h"
+// #include "barney/amr/CUBQLBlockSampler.h"
 
-namespace barney {
+// namespace barney {
 
-  /*! a macrocell accelerator built over AMR blocks */
-  template<typename FieldSampler>
-  struct BlockStructuredMCAccelerator : public MCAccelerator<FieldSampler>
-  {
-    using MCAccelerator<FieldSampler>::mcGrid;
-    using MCAccelerator<FieldSampler>::volume;
+//   /*! a macrocell accelerator built over AMR blocks */
+//   template<typename FieldSampler>
+//   struct BlockStructuredMCAccelerator : public MCAccelerator<FieldSampler>
+//   {
+//     using MCAccelerator<FieldSampler>::mcGrid;
+//     using MCAccelerator<FieldSampler>::volume;
 
-    struct DD : public MCAccelerator<FieldSampler>::DD {
-      using MCAccelerator<FieldSampler>::DD::sampleAndMap;
-    };
+//     struct DD : public MCAccelerator<FieldSampler>::DD {
+//       using MCAccelerator<FieldSampler>::DD::sampleAndMap;
+//     };
 
-    BlockStructuredMCAccelerator(BlockStructuredField *field, Volume *volume)
-      : MCAccelerator<FieldSampler>(field,volume),
-        field(field)
-    {}
-    static OWLGeomType createGeomType(DevGroup *devGroup);
+//     BlockStructuredMCAccelerator(BlockStructuredField *field, Volume *volume)
+//       : MCAccelerator<FieldSampler>(field,volume),
+//         field(field)
+//     {}
+//     static OWLGeomType createGeomType(DevGroup *devGroup);
 
-    void build(bool full_rebuild) override;
+//     void build(bool full_rebuild) override;
 
-    OWLGeom geom = 0;
+//     OWLGeom geom = 0;
 
-    BlockStructuredField *const field;
-  };
+//     BlockStructuredField *const field;
+//   };
 
-  typedef BlockStructuredMCAccelerator<CUBQLBlockSampler> BlockStructuredAccel_MC_CUBQL;
-}
+//   typedef BlockStructuredMCAccelerator<CUBQLBlockSampler> BlockStructuredAccel_MC_CUBQL;
+// }
