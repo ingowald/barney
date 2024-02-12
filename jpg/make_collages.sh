1431a9ed0dce8be980fb1271093252db5009da92
Ingo Wald <ingowald@gmail.com>
2024-02-11 23:10:49 -0700

    fixed collages

--- jpg/make_collages.sh
+++ jpg/make_collages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-convert -resize x500! ls.jpg ls-pond.jpg +append ls-collage.jpg
+convert -resize x500! ls.jpg  -resize x500! ls-pond.jpg   -resize x500! +append ls-collage.jpg
 
 
-convert -resize x500! engine.jpg rotstrat-fuzzy.jpg rotstrat-fuzzy.jpg +append structured-collage.jpg
+convert -resize x500 engine.jpg  -resize x500 rotstrat-fuzzy.jpg  -resize x500 rotstrat-dense.jpg   -resize x500 +append structured-collage.jpg
