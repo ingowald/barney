d3fa2445d86c99aa280fdf85354ab4de0ee5f9d1
Stefan Zellmann <info@szellmann.de>
2024-03-20 23:00:59 +0100

    Refactor makeBarneyData() to support all array types

--- anari/common.h
+++ anari/common.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include <vector>
-#include "helium/array/Array1D.h"
+#include "helium/array/Array.h"
 #include "barney_math.h"
 
 namespace barney_device {
@@ -44,48 +44,74 @@ inline WrapMode toWrapMode(std::string str) {
   return Clamp;
 }
 
+inline void convert_fixed4_to_float4(const void *in, math::float4 *out, size_t size)
+{
+  for (size_t i = 0; i < size; ++i) {
+    out[i].x = ((const math::byte4 *)in)[i].x / 255.f;
+    out[i].y = ((const math::byte4 *)in)[i].y / 255.f;
+    out[i].z = ((const math::byte4 *)in)[i].z / 255.f;
+    out[i].w = ((const math::byte4 *)in)[i].w / 255.f;
+  }
+}
+
+inline void convert_float1_to_float4(const float *in, math::float4 *out, size_t size)
+{
+  for (size_t i = 0; i < size; ++i) {
+    out[i].x = in[i];
+    out[i].y = 0.f;
+    out[i].z = 0.f;
+    out[i].w = 0.f;
+  }
+}
+
+inline void convert_float2_to_float4(const math::float2 *in, math::float4 *out, size_t size)
+{
+  for (size_t i = 0; i < size; ++i) {
+    out[i].x = in[i].x;
+    out[i].y = in[i].y;
+    out[i].z = 0.f;
+    out[i].w = 0.f;
+  }
+}
+
+inline void convert_float3_to_float4(const math::float3 *in, math::float4 *out, size_t size)
+{
+  for (size_t i = 0; i < size; ++i) {
+    out[i].x = in[i].x;
+    out[i].y = in[i].y;
+    out[i].z = in[i].z;
+    out[i].w = 0.f;
+  }
+}
+
 static BNData makeBarneyData(
-    BNModel model, int slot, const helium::IntrusivePtr<helium::Array1D> &input)
+    BNModel model, int slot, const helium::IntrusivePtr<helium::Array> &input)
 {
   BNData res{0};
 
   if (input) {
     if (input->elementType() == ANARI_UFIXED8_VEC4) {
-      std::vector<math::float4> data(input->size(), math::float4(0.f, 0.f, 0.f, 1.f));
-      for (size_t i = 0; i < data.size(); ++i) {
-        data[i].x = ((const math::byte4 *)input->data())[i].x / 255.f;
-        data[i].y = ((const math::byte4 *)input->data())[i].y / 255.f;
-        data[i].z = ((const math::byte4 *)input->data())[i].z / 255.f;
-        data[i].w = ((const math::byte4 *)input->data())[i].w / 255.f;
-      }
+      std::vector<math::float4> data(input->totalSize());
+      convert_fixed4_to_float4(input->data(), data.data(), data.size());
       res = bnDataCreate(model, slot, BN_FLOAT4, data.size(), data.data());
     }
     else if (input->elementType() == ANARI_FLOAT32) {
-      std::vector<math::float4> data(input->size(), math::float4(0.f, 0.f, 0.f, 1.f));
-      for (size_t i = 0; i < data.size(); ++i) {
-        data[i].x = input->beginAs<float>()[i];
-      }
+      std::vector<math::float4> data(input->totalSize());
+      convert_float1_to_float4(input->dataAs<float>(), data.data(), data.size());
       res = bnDataCreate(model, slot, BN_FLOAT4, data.size(), data.data());
     }
     else if (input->elementType() == ANARI_FLOAT32_VEC2) {
-      std::vector<math::float4> data(input->size(), math::float4(0.f, 0.f, 0.f, 1.f));
-      for (size_t i = 0; i < data.size(); ++i) {
-        data[i].x = input->beginAs<math::float2>()[i].x;
-        data[i].y = input->beginAs<math::float2>()[i].y;
-      }
+      std::vector<math::float4> data(input->totalSize());
+      convert_float2_to_float4(input->dataAs<math::float2>(), data.data(), data.size());
       res = bnDataCreate(model, slot, BN_FLOAT4, data.size(), data.data());
     }
     else if (input->elementType() == ANARI_FLOAT32_VEC3) {
-      std::vector<math::float4> data(input->size(), math::float4(0.f, 0.f, 0.f, 1.f));
-      for (size_t i = 0; i < data.size(); ++i) {
-        data[i].x = input->beginAs<math::float3>()[i].x;
-        data[i].y = input->beginAs<math::float3>()[i].y;
-        data[i].z = input->beginAs<math::float3>()[i].z;
-      }
+      std::vector<math::float4> data(input->totalSize());
+      convert_float3_to_float4(input->dataAs<math::float3>(), data.data(), data.size());
       res = bnDataCreate(model, slot, BN_FLOAT4, data.size(), data.data());
     }
     else if (input->elementType() == ANARI_FLOAT32_VEC4) {
-      res = bnDataCreate(model, slot, BN_FLOAT4, input->size(), input->begin());
+      res = bnDataCreate(model, slot, BN_FLOAT4, input->totalSize(), input->data());
     }
     else {
       std::stringstream ss;
