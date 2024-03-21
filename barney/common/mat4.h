a403c27834557bc8624bcc5227b4cb1dd9d8ddb5
Stefan Zellmann <info@szellmann.de>
2024-03-21 21:32:02 +0100

    Add operator<< for mat4f

--- barney/common/mat4.h
+++ barney/common/mat4.h
@@ -17,6 +17,8 @@
 #pragma once
 
 #include "barney-common.h"
+// std
+#include <ostream>
 
 namespace barney {
 
@@ -36,6 +38,15 @@ namespace barney {
       dot(vec4f(m.e[ 8],m.e[ 9],m.e[10],m.e[11]),v),
       dot(vec4f(m.e[12],m.e[13],m.e[14],m.e[15]),v)
     };
+  }
+
+  inline std::ostream &operator<<(std::ostream &out, const mat4f &m)
+  {
+    out << '(' << m.e[ 0] << ',' << m.e[ 1] << ',' << m.e[ 2] << ',' << m.e[ 3] << ')'
+        << '(' << m.e[ 4] << ',' << m.e[ 5] << ',' << m.e[ 6] << ',' << m.e[ 7] << ')'
+        << '(' << m.e[ 8] << ',' << m.e[ 9] << ',' << m.e[10] << ',' << m.e[11] << ')'
+        << '(' << m.e[12] << ',' << m.e[13] << ',' << m.e[14] << ',' << m.e[15] << ')';
+    return out;
   }
 } // barney
 
