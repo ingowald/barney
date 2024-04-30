f43434700d16caf7e93f2844da8bfe7f39a8784b
Ingo Wald <ingowald@gmail.com>
2024-04-30 12:24:29 -0600

    added missing world.cpp

---
+++ barney/render/World.cpp
@@ -0,0 +1,110 @@
+// ======================================================================== //
+// Copyright 2023-2024 Ingo Wald                                            //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include "barney/render/World.h"
+
+namespace barney {
+  namespace render {
+
+    MaterialLibrary::MaterialLibrary(DevGroup *devGroup)
+      : devGroup(devGroup)
+    {
+      numReserved = 1;
+      buffer = owlDeviceBufferCreate
+        (devGroup->owl,OWL_USER_TYPE(render::DeviceMaterial),1,nullptr);
+    }
+
+    MaterialLibrary::~MaterialLibrary()
+    {
+      owlBufferRelease(buffer);
+    }
+    
+    void MaterialLibrary::grow()
+    {
+      numReserved *= 2;
+      owlBufferResize(buffer,numReserved);
+    }
+
+    int MaterialLibrary::allocate()
+    {
+      if (!reusableIDs.empty()) {
+        int ID = reusableIDs.top();
+        reusableIDs.pop();
+        return ID;
+      }
+      if (nextFree == numReserved) grow();
+
+      return nextFree++;
+    }
+   
+    void MaterialLibrary::release(int nowReusableID)
+    {
+      reusableIDs.push(nowReusableID);
+    }
+  
+    const render::device::Material *MaterialLibrary::getPointer(int owlDeviceID) const 
+    {
+      return (render::device::Material *)owlBufferGetPointer(buffer,owlDeviceID);
+    }    
+
+
+
+
+
+    SamplerLibrary::SamplerLibrary(DevGroup *devGroup)
+      : devGroup(devGroup)
+    {
+      numReserved = 1;
+      buffer = owlDeviceBufferCreate
+        (devGroup->owl,OWL_USER_TYPE(render::Sampler::DD),1,nullptr);
+    }
+
+    SamplerLibrary::~SamplerLibrary()
+    {
+      owlBufferRelease(buffer);
+    }
+    
+    void SamplerLibrary::grow()
+    {
+      numReserved *= 2;
+      owlBufferResize(buffer,numReserved);
+    }
+
+    int SamplerLibrary::allocate()
+    {
+      if (!reusableIDs.empty()) {
+        int ID = reusableIDs.top();
+        reusableIDs.pop();
+        return ID;
+      }
+      if (nextFree == numReserved) grow();
+
+      return nextFree++;
+    }
+   
+    void SamplerLibrary::release(int nowReusableID)
+    {
+      reusableIDs.push(nowReusableID);
+    }
+  
+    const render::device::Sampler *SamplerLibrary::getPointer(int owlDeviceID) const
+    {
+      return (render::device::Sampler *)owlBufferGetPointer(buffer,owlDeviceID);
+    }    
+
+    
+  }
+}
