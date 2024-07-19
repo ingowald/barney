ab1f1f37fdb57c3993326fcefb8aa1c8a910e412
Jefferson Amstutz <jamstutz@nvidia.com>
2024-07-19 10:18:51 -0500

    fix for windows builds

--- barney/render/DG.h
+++ barney/render/DG.h
@@ -56,8 +56,8 @@ namespace barney {
       vec3f value;
       float pdf;
     };
-    
-    
+
+
     struct SampleRes {
       // inline __device__ SampleRes() {}
       // inline __device__ SampleRes(vec3f v, float p) : value(v),pdf(p) {}
@@ -82,7 +82,7 @@ namespace barney {
       //   GLOSSY_TRANS   = (1<<5),
       // } Type;
       inline __device__ bool valid() const    { return pdf > 0.f && !isinf(pdf); }
-      
+
       vec3f f_r;
       vec3f dir;
       float pdf  = 0.f;
@@ -90,8 +90,8 @@ namespace barney {
       bool  changedMedium = false;
       float offsetDirection = +1.f;
     };
-    
-    
+
+
     inline __device__
     vec3f sampleCosineWeightedHemisphere(vec3f Ns, Random &random)
     {
@@ -103,48 +103,53 @@ namespace barney {
                          );
       }
     }
-  
+
   inline __device__ float pbrt_clampf(float f, float lo, float hi)
   { return max(lo,min(hi,f)); }
-  
+
   inline __device__ float pbrtSphericalTheta(const vec3f &v)
   {
     return acosf(pbrt_clampf(v.z, -1.f, 1.f));
   }
-  
+
   inline __device__ float pbrtSphericalPhi(const vec3f &v)
   {
     float p = atan2f(v.y, v.x);
     return (p < 0.f) ? (p + float(2.f * M_PI)) : p;
   }
 
-    
+
     inline __device__
     float luminance(vec3f c)
     { return 0.212671f*c.x + 0.715160f*c.y + 0.072169f*c.z; }
 
 
 
-    inline __device__ 
+    inline __device__
     vec3f cartesian(float phi, float sinTheta, float cosTheta)
     {
       float sinPhi, cosPhi;
+#ifdef _WIN32
+      sinPhi = sinf(phi);
+      cosPhi = cosf(phi);
+#else
       sincosf(phi, &sinPhi, &cosPhi);
+#endif
       return vec3f(cosPhi * sinTheta,
                    sinPhi * sinTheta,
                    cosTheta);
     }
-    
-    inline __device__ 
+
+    inline __device__
     vec3f cartesian(const float phi, const float cosTheta)
     {
       return cartesian(phi, cos2sin(cosTheta), cosTheta);
     }
-    
 
 
-    
-    inline __device__ 
+
+
+    inline __device__
     vec3f cosineSampleHemisphere(const vec2f s)
     {
       const float phi = two_pi * s.x;
@@ -152,18 +157,18 @@ namespace barney {
       const float sinTheta = sqrtf(1.0f - s.y);
       return cartesian(phi, sinTheta, cosTheta);
     }
-    
-    inline __device__ 
+
+    inline __device__
     float cosineSampleHemispherePDF(const vec3f &dir)
     {
       return dir.z * one_over_pi;
     }
 
-    inline __device__ 
+    inline __device__
     float cosineSampleHemispherePDF(float cosTheta)
     {
       return cosTheta * one_over_pi;
     }
-    
+
   }
 }
