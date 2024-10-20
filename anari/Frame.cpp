7952b111065566484531b1766ad612d40f759d0e
Ingo Wald <ingowald@gmail.com>
2024-10-19 19:31:52 -0600

    banari no longer adds 1/10 factor in ambient radiance

--- anari/Frame.cpp
+++ anari/Frame.cpp
@@ -136,7 +136,13 @@ void Frame::renderFrame()
 
   const int pixelSamples = std::max(m_renderer->pixelSamples(), 1);
   const float radiance = m_renderer->radiance();
-  bnSetRadiance(model, 0, radiance / 10.f);
+  bnSetRadiance(model, 0, radiance
+#if 0
+                /* iw- no idea where this factor came from, but it
+                   probably shouldn't be here */
+                / 10.f
+#endif
+                );
 
   bnRender(model, m_camera->barneyCamera(), m_bnFrameBuffer, pixelSamples);
 
