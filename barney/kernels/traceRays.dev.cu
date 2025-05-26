061e053d4fe23ce538af363086b175bebf9a9b0e
Ingo Wald <ingowald@gmail.com>
2025-05-26 12:54:32 -0600

    removed superfluous dbg statement

--- barney/kernels/traceRays.dev.cu
+++ barney/kernels/traceRays.dev.cu
@@ -55,12 +55,6 @@ namespace BARNEY_NS {
       if (dir.y == 0.f) dir.y = 1e-6f;
       if (dir.z == 0.f) dir.z = 1e-6f;
 
-#ifdef NDEBUG
-      bool dbg = false;
-#else
-      bool dbg = 0 != (int)ray.dbg;
-#endif
-
       ti.traceRay(lp.accel,
                   ray.org,
                   dir,
