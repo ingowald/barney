51600fcef47de876cd278259e4f6dd88d39dd003
Ingo Wald <ingowald@gmail.com>
2025-08-07 15:30:32 -0600

    data parallel working again

--- barney/api/barney.cu
+++ barney/api/barney.cu
@@ -976,8 +976,6 @@ namespace barney_api {
 	    _gpuIDs = &negOne;
 	    numGPUs = 1;
     }
-    const bool userSuppliedGpuListWasEmpty = (_gpuIDs == nullptr);
-
     mpi::Comm world(_comm);
     if (world.size == 1) {
       // std::cout << "#bn: MPIContextInit, but only one rank - using
@@ -1046,80 +1044,81 @@ namespace barney_api {
                                "but optix backend not compiled in");
 #endif
     }
+    PRINT(_gpuIDs);
     throw std::runtime_error("barney mpi-parallel without a list of GPUs is no longer supporteed");
   }
 
 
-  BARNEY_API
-  void  bnMPIQueryHardware(BNHardwareInfo *_hardware, MPI_Comm _comm)
-  {
-    LOG_API_ENTRY;
+//   BARNEY_API
+//   void  bnMPIQueryHardware(BNHardwareInfo *_hardware, MPI_Comm _comm)
+//   {
+//     LOG_API_ENTRY;
 
-    assert(_hardware);
-    BNHardwareInfo &hardware = *_hardware;
+//     assert(_hardware);
+//     BNHardwareInfo &hardware = *_hardware;
 
-    assert(_comm != MPI_COMM_NULL);
-    barney_api::mpi::Comm comm(_comm);
+//     assert(_comm != MPI_COMM_NULL);
+//     barney_api::mpi::Comm comm(_comm);
 
-    hardware.numRanks = comm.size;
-    char hostName[MPI_MAX_PROCESSOR_NAME];
-    memset(hostName,0,MPI_MAX_PROCESSOR_NAME);
-    int hostNameLen = 0;
-    BN_MPI_CALL(Get_processor_name(hostName,&hostNameLen));
+//     hardware.numRanks = comm.size;
+//     char hostName[MPI_MAX_PROCESSOR_NAME];
+//     memset(hostName,0,MPI_MAX_PROCESSOR_NAME);
+//     int hostNameLen = 0;
+//     BN_MPI_CALL(Get_processor_name(hostName,&hostNameLen));
 
-    std::vector<char> recvBuf(MPI_MAX_PROCESSOR_NAME*comm.size);
-    memset(recvBuf.data(),0,recvBuf.size());
+//     std::vector<char> recvBuf(MPI_MAX_PROCESSOR_NAME*comm.size);
+//     memset(recvBuf.data(),0,recvBuf.size());
 
-    // ------------------------------------------------------------------
-    // determine which (world) rank lived on which host, and assign
-    // GPUSs
-    // ------------------------------------------------------------------
-    BN_MPI_CALL(Allgather(hostName,
-                          MPI_MAX_PROCESSOR_NAME,MPI_CHAR,
-                          recvBuf.data(),
-                          /* PER rank size */MPI_MAX_PROCESSOR_NAME,MPI_CHAR,
-                          comm.comm));
-    std::vector<std::string>  hostNames;
-    std::map<std::string,int> ranksOnHost;
-    for (int i=0;i<comm.size;i++)  {
-      std::string host_i = recvBuf.data()+i*MPI_MAX_PROCESSOR_NAME;
-      hostNames.push_back(host_i);
-      ranksOnHost[host_i] ++;
-    }
+//     // ------------------------------------------------------------------
+//     // determine which (world) rank lived on which host, and assign
+//     // GPUSs
+//     // ------------------------------------------------------------------
+//     BN_MPI_CALL(Allgather(hostName,
+//                           MPI_MAX_PROCESSOR_NAME,MPI_CHAR,
+//                           recvBuf.data(),
+//                           /* PER rank size */MPI_MAX_PROCESSOR_NAME,MPI_CHAR,
+//                           comm.comm));
+//     std::vector<std::string>  hostNames;
+//     std::map<std::string,int> ranksOnHost;
+//     for (int i=0;i<comm.size;i++)  {
+//       std::string host_i = recvBuf.data()+i*MPI_MAX_PROCESSOR_NAME;
+//       hostNames.push_back(host_i);
+//       ranksOnHost[host_i] ++;
+//     }
 
-    hardware.numRanksThisHost = ranksOnHost[hostName];
-    hardware.numHosts         = ranksOnHost.size();
+//     hardware.numRanksThisHost = ranksOnHost[hostName];
+//     hardware.numHosts         = ranksOnHost.size();
 
-    // ------------------------------------------------------------------
-    // count how many other ranks are already on this same node
-    // ------------------------------------------------------------------
-    BN_MPI_CALL(Barrier(comm.comm));
-    int localRank = 0;
-    for (int i=0;i<comm.rank;i++)
-      if (hostNames[i] == hostName)
-        localRank++;
-    BN_MPI_CALL(Barrier(comm.comm));
-    hardware.localRank = localRank;
-    hardware.numRanksThisHost = ranksOnHost[hostName];
+//     // ------------------------------------------------------------------
+//     // count how many other ranks are already on this same node
+//     // ------------------------------------------------------------------
+//     BN_MPI_CALL(Barrier(comm.comm));
+//     int localRank = 0;
+//     for (int i=0;i<comm.rank;i++)
+//       if (hostNames[i] == hostName)
+//         localRank++;
+//     BN_MPI_CALL(Barrier(comm.comm));
+//     hardware.localRank = localRank;
+//     hardware.numRanksThisHost = ranksOnHost[hostName];
 
-    // ------------------------------------------------------------------
-    // assign a GPU to this rank
-    // ------------------------------------------------------------------
-    int numGPUsOnThisHost = 0;
-#if BARNEY_BACKEND_OPTIX
-    cudaGetDeviceCount(&numGPUsOnThisHost);
-#endif
-    // cudaGetDeviceCount(&numGPUsOnThisHost);
-    // if (numGPUsOnThisHost == 0)
-    //   throw std::runtime_error("no barney-capable devices on this rank!");
-    hardware.numGPUsThisHost = numGPUsOnThisHost;
-    hardware.numGPUsThisRank
-      = comm.allReduceMin(hardware.numGPUsThisHost == 0
-                          ? 0
-                          : std::max(hardware.numGPUsThisHost/
-                                     hardware.numRanksThisHost,
-                                     1));
-  }
+//     // ------------------------------------------------------------------
+//     // assign a GPU to this rank
+//     // ------------------------------------------------------------------
+//     int numGPUsOnThisHost = 0;
+// #if BARNEY_BACKEND_OPTIX
+//     cudaGetDeviceCount(&numGPUsOnThisHost);
+// #endif
+//     // cudaGetDeviceCount(&numGPUsOnThisHost);
+//     // if (numGPUsOnThisHost == 0)
+//     //   throw std::runtime_error("no barney-capable devices on this rank!");
+//     hardware.numGPUsThisHost = numGPUsOnThisHost;
+//     hardware.numGPUsThisRank
+//       = comm.allReduceMin(hardware.numGPUsThisHost == 0
+//                           ? 0
+//                           : std::max(hardware.numGPUsThisHost/
+//                                      hardware.numRanksThisHost,
+//                                      1));
+//   }
   
 #endif
-} // ::owl
+} // ::barney_api
