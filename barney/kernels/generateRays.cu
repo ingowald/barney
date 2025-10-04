9c1e47ce4f0fa9cb6b2de67130920bba79c32816
Ingo Wald <ingowald@gmail.com>
2025-10-04 13:43:37 -0600

    bugfix: denoiser didn't properly sync streams with copying pixels, sometimes ran before pixels were ready

--- barney/kernels/generateRays.cu
+++ barney/kernels/generateRays.cu
@@ -138,19 +138,8 @@ namespace BARNEY_NS {
       // ray._dbg         = 0;
       ray.crosshair   = 0;
 #else
-# if 0
       bool crossHair_x = (ix == fbSize.x/2);
       bool crossHair_y = (iy == fbSize.y/2);
-# else
-#  if 0
-      // MISS
-      bool crossHair_x = (ix == 864);
-#  else
-      // HIT:
-      bool crossHair_x = (ix == 867);
-#  endif
-      bool crossHair_y = (iy == 205);
-# endif
       ray._dbg         = enablePerRayDebug && (crossHair_x && crossHair_y);
       ray.crosshair
         = enablePerRayDebug && (crossHair_x || crossHair_y);
